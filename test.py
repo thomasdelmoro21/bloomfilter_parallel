3a75dd915611e47bc1dff22ec2f55bdad872b659
loreb <lorebaia@gmail.com>
2023-10-07 12:29:32 +0200

    final bloom filter

--- test.py
+++ test.py
@@ -9,10 +9,11 @@ import numpy as np
 
 # Number of cores/threads
 n_threads = os.cpu_count()  # Number of cores/threads
-test_threads = np.linspace(2, n_threads, 4, dtype=int)  # Number of threads to test
+test_threads = np.linspace(2, n_threads, 8, dtype=int)  # Number of threads to test
 
 # Number of emails to test
-test_sizes = np.logspace(4, 8, 10, base=10, dtype=int)  # Number of emails to test
+test_sizes = np.linspace(1e4, 1e8, 50, dtype=int)  # Number of emails to test
+# test_sizes = np.logspace(4, 7, 50, base=10, dtype=int)  # Number of emails to test
 
 # Bloom Filter
 fpr = 0.01  # False Positive Rate
